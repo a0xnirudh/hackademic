3d3f5dafc87036ef7d91bdeb8880c988cc55dd80
a0xnirudh <anirudhanand722@gmail.com>
2016-06-01 15:57:18 +0530

    Minor bug fixes in communicator

--- ContainerManager/communicator.py
+++ ContainerManager/communicator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-
+import sys
 import json
 import socket
 
--- ContainerManager/test.php
+++ ContainerManager/test.php
@@ -1,5 +1,5 @@
 <?php
-$abc = exec('python test.py create_container webchallenge1');
+$abc = exec('python test.py create_container samplechallenge');
 echo $abc;
 header("Location: ".$abc);
 exit(0);
